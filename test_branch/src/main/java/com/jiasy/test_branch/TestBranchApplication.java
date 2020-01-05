a52519d8330db7aba685eec072d88458d9e34a1d
j8130 <122969720@qq.com>
2020-01-05 18:01:00 +0800

    Initial commit

---
+++ test_branch/src/main/java/com/jiasy/test_branch/TestBranchApplication.java
@@ -0,0 +1,13 @@
+package com.jiasy.test_branch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestBranchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestBranchApplication.class, args);
+    }
+
+}
---
+++ test_branch/src/test/java/com/jiasy/test_branch/TestBranchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jiasy.test_branch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestBranchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
