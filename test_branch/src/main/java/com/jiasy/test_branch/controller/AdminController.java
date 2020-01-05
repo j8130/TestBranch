04e8a5a48e27e925d8396dbbe82e1c20575d60c2
j8130 <122969720@qq.com>
2020-01-05 18:07:11 +0800

    创建分支

--- test_branch/src/main/java/com/jiasy/test_branch/controller/AdminController.java
+++ test_branch/src/main/java/com/jiasy/test_branch/controller/AdminController.java
@@ -10,5 +10,7 @@ import org.springframework.stereotype.Controller;
 public class AdminController {
     public static void main(String[] args) {
 
+        System.out.println("This is dev branch");
+
     }
 }
