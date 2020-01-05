501173dbacb8015479950d6978a1323f63f3fd6c
j8130 <122969720@qq.com>
2020-01-05 18:04:33 +0800

    controller

---
+++ test_branch/src/main/java/com/jiasy/test_branch/controller/AdminController.java
@@ -0,0 +1,14 @@
+package com.jiasy.test_branch.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * @Author: jsy
+ * @Date: 2020/1/5 18:02
+ */
+@Controller
+public class AdminController {
+    public static void main(String[] args) {
+
+    }
+}
